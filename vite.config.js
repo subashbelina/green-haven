8e23ed0a9319108163688a0db2bfde6a5e82f9f5
noobcodxsb <subashbelina255@gmail.com>
2025-05-01 20:03:47 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/green-haven/' : '/',
+  base: '/green-haven/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
